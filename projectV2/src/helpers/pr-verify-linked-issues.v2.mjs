f12220b98564add1e7898c67e533db3a0616903e
chasovskiy_s <rs.chasovskiys@gmail.com>
2026-02-16 11:55:43 +0200

    [codex] refactor code structure

--- projectV2/src/helpers/pr-verify-linked-issues.v2.mjs
+++ projectV2/src/helpers/pr-verify-linked-issues.v2.mjs
@@ -4,7 +4,7 @@ import {
     buildWarningBlock,
     extractIssues,
     stripAutoBlocks
-} from "../helpers/pr-verify-linked-issues.helpers.mjs";
+} from "./pr-verify-linked-issues.helpers.mjs";
 
 async function getPrBody(github, { owner, repo, prNumber }) {
     const { data: pr } = await github.rest.pulls.get({
