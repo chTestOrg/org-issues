f9e87128fbd178c7acd7fb8a6f7949b774cd061f
chasovskiy_s <rs.chasovskiys@gmail.com>
2026-03-26 13:44:11 +0200

    processEvent

--- projectV2/workflow-handler.mjs
+++ projectV2/workflow-handler.mjs
@@ -7,19 +7,27 @@ import prEdited from "./workflows/pr-edited.mjs";
 export default async function processEvent({github, context, core, githubToken}) {
     await logGroup(core, "GitHub Project Automation", async () => {
         const {action, pull_request, sender} = context.payload;
-        const repoName = context.repo.repo;
 
-        if (sender.type === 'Bot') {
-            core.info("🤖 Action triggered by a bot. Exiting silently to keep status.");
+        const repoName = context.repo.repo;
+        const prNumber = pull_request?.number;
+        const prTitle = pull_request?.title;
+
+        core.info(`🚀 Processing event pull request: "${action}" for "${repoName}"`);
+
+        core.info(`githubApp Token exists: ${github ? "YES" : "NO"}`)
+        core.info(`githubAction Token exists: ${githubToken ? "YES" : "NO"}`)
+
+        core.info(`🔧 Pull request number: #${prNumber} - ${prTitle}`);
+        core.info(`🔧 Pull request author: ${pull_request?.user?.type}`);
+        core.info(`👤 Sender type: "${sender.type}" login "${sender.login}"`);
+        core.info(`🌿 Source branch: ${pull_request.head.ref}`);
+        core.info(`🎯 Target branch: ${pull_request.base.ref}`);
+        core.info(`📦 Number of commits: ${pull_request.commits}`);
+
+        if (sender.type === 'Bot' || pull_request?.user?.type === 'Bot') {
+            core.info("🤖 Action triggered by a bot. Exiting silently.");
             return;
         }
-
-        // Початковий статус
-        core.info(`🚀 Processing ${action} for ${repoName}`);
-        core.info(`>>>>>>>>>>>>>>>>>>>>>>>`)
-        core.info(`Token exists: ${githubToken ? "YES" : "NO"}`)
-        core.info(`Token length: ${githubToken?.length}`)
-
         // Виводимо весь payload у форматі JSON
         core.info("Full context.payload:");
         core.info(JSON.stringify(context.payload, null, 2));
